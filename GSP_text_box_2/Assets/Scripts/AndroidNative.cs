62c1a13743eaff71707959ed7af1dcc0c707e820
seungyeol.son <seungyeol.son@sundaytoz.com>
2022-02-03 17:35:30 +0900

    Added Unity native Android popup - setup

---
+++ GSP_text_box_2/Assets/Scripts/AndroidNative.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidNative
+{
+    private static AndroidJavaObject activityContext;
+    private static AndroidJavaClass javaClass;
+    private static AndroidJavaObject javaClassInstance;
+
+    public static void SetTest()
+    {
+        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+            Debug.Log("activityContext :    " + activityContext);
+        }
+
+        using (javaClass = new AndroidJavaClass("com.example.gsp_toast.AndroidNative"))
+        {
+            
+            if (javaClass != null)
+            {
+                
+                Debug.Log("JavaClass :    " + javaClass);
+                javaClassInstance = javaClass.CallStatic<AndroidJavaObject>("instance");
+                Debug.Log("JavaClassInstance :    " + javaClassInstance);
+                javaClassInstance.Call("setContext" , activityContext);
+            }
+        }
+    }
+
+    public static void CallShowToast(string text)
+    {
+        activityContext.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            javaClassInstance.Call("ShowToast", text);
+        }));
+    }
+    
+}
---
+++ GSP_text_box_2/Assets/Scripts/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Text asd;
+
+    private void Awake()
+    {
+        try
+        {
+            AndroidNative.SetTest();
+        }
+        catch (Exception e)
+        {
+            asd.text += e;
+            
+        }
+    }
+
+    public void OnToast(InputField text)
+    {
+        try
+        {
+            AndroidNative.CallShowToast(text.textComponent.text);
+        }
+        catch (Exception e)
+        {
+            asd.text += "\n" + e;
+        }
+
+        asd.text += "\n" + text.textComponent.text;
+        Debug.Log(text.textComponent.text);
+    }
+    
+}
